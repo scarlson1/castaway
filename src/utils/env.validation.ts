1fe7361adfc6d237a509935c7d6216306513b6f6
Spencer Carlson <19680614+scarlson1@users.noreply.github.com>
2025-12-01 09:55:59 -0600

    sentry dns env var

--- src/utils/env.validation.ts
+++ src/utils/env.validation.ts
@@ -16,6 +16,7 @@ const envSchema = z.object({
   VITE_CONVEX_URL: z.string(),
   CLERK_SIGN_IN_URL: z.string().optional(),
   CLERK_SIGN_UP_URL: z.string().optional(),
+  VITE_SENTRY_DNS: z.string(),
   // COOKIE_SECRET: z.string(),
 });
 
