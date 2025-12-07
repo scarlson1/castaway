7395124c6e348af702be29433471635bd35c08dd
Spencer Carlson <19680614+scarlson1@users.noreply.github.com>
2025-12-07 13:10:29 -0600

    favicons

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -104,10 +104,39 @@ export const Route = createRootRouteWithContext<RouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start',
+        name: 'description',
+        content: 'Castaway ad free with AI ad detection for any podcast',
+      },
+      {
+        title: 'Castaway',
+      },
+      { name: 'keywords', content: 'podcast,ai,ad free,chatGPT' },
+    ],
+    links: [
+      { rel: 'stylesheet', href: fontsourceVariableRobotoCss },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
       },
     ],
-    links: [{ rel: 'stylesheet', href: fontsourceVariableRobotoCss }],
   }),
   component: RootComponent,
   // notFoundComponent: () => <NotFound />,
